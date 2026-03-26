8d8a8558f2887a22d8f3f9a323e9e0600f611330
刻夜 <2250919876@qq.com>
2026-03-27 06:56:07 +0800

    1.等待数据加载完成再进入场景 2.IL2CPP

--- UnityProject/Assets/GameScripts/Procedure/ProcedureReadPersistentInfo.cs
+++ UnityProject/Assets/GameScripts/Procedure/ProcedureReadPersistentInfo.cs
@@ -35,7 +35,7 @@ namespace Procedure
             await GameModule.Scene.LoadSceneAsync("Loading", LoadSceneMode.Single, false);
             var uiWait = await GameModule.UI.ShowUIAsyncAwait<UI_Wait>();
             // todo:加载资源
-            await GameModule.Scene.LoadSceneAsync(procedureOwner.GetData<string>("NextSceneName"), LoadSceneMode.Single, false);
+            await GameModule.Scene.LoadSceneAsync(procedureOwner.GetData<string>("NextSceneName"), LoadSceneMode.Single, true);
             GameModule.UI.CloseUI<UI_Wait>();
             // todo: ChangeState<PlayerRoom>(procedureOwner);
         }
